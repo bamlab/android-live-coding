244fed44bb970db13967a643fb83d6bebfc2abd8
Thomas Pucci <thomas.lb.pucci@gmail.com>
2021-06-25 11:05:48 +0200

    feat: add Instagram screen texts

--- app/src/main/java/tech/bam/livecoding/ui/theme/instagram/InstagramScreen.kt
+++ app/src/main/java/tech/bam/livecoding/ui/theme/instagram/InstagramScreen.kt
@@ -1,20 +1,28 @@
 package tech.bam.livecoding.ui.theme.instagram
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import tech.bam.livecoding.ui.theme.BlueSea
 import tech.bam.livecoding.ui.theme.GreenLeaves
 import tech.bam.livecoding.ui.theme.GreenLemon
+import tech.bam.livecoding.ui.theme.Typography
 
 @Composable
 fun InstagramScreen() {
     Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier =
         Modifier
             .background(
                 Brush.linearGradient(
@@ -24,7 +32,13 @@ fun InstagramScreen() {
             )
             .fillMaxSize()
     ) {
-
+        Text(text = "How you doin' ?", style = Typography.h1, color = Color.White)
+        Text(
+            text = "Tap or wait to go to the next screen",
+            style = Typography.body1,
+            color = Color.White
+        )
+        Text(text = "👉", style = Typography.body1, color = Color.White)
     }
 }
 
