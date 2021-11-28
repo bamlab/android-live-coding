a7f50bcae18a37693a53f0ecfdbab9ee59266a61
Thomas Pucci <thomas.lb.pucci@gmail.com>
2021-11-28 12:42:59 +0100

    feat: add checkbox

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -44,9 +44,12 @@ android {
 
 dependencies {
     implementation("com.google.android.material:material:1.3.0")
+    implementation("com.airbnb.android:lottie-compose:4.2.1")
     implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0-beta02")
+
     testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
--- ui/src/main/java/tech/bam/ui/Checkbox.kt
+++ ui/src/main/java/tech/bam/ui/Checkbox.kt
@@ -1,18 +1,28 @@
 package tech.bam.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieAnimatable
+import com.airbnb.lottie.compose.rememberLottieComposition
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -27,8 +37,10 @@ fun Checkbox() {
         val isChecked = clicks
             .scan(initial = false) { isChecked, _ -> !isChecked }
             .collectAsState(initial = false)
-
-        CheckboxFeedback(isChecked = isChecked)
+        
+        Stars(enabled = isChecked) {
+            CheckboxFeedback(isChecked = isChecked)
+        }
     }
 }
 
@@ -38,16 +50,60 @@ fun ClickHandler(content: @Composable (clicks: Flow<Unit>) -> Unit) {
     val clicks = remember { MutableSharedFlow<Unit>() }
 
     Box(modifier = Modifier
-        .clickable { composableScope.launch { clicks.emit(Unit) } }) {
+        .clickable { composableScope.launch { clicks.emit(Unit) } }
+    ) {
         content(clicks)
     }
 }
 
 @Composable
 fun CheckboxFeedback(isChecked: State<Boolean>) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.check))
+    val lottieAnimatable = rememberLottieAnimatable()
+
+    LaunchedEffect(isChecked.value) {
+        lottieAnimatable.animate(
+            composition = composition,
+            speed = if (isChecked.value) 2f else -2f
+        )
+    }
+
     Box(
-        modifier = Modifier
-            .size(600.dp)
-            .background(if (isChecked.value) Color.Green else Color.Red)
-    )
+        modifier = Modifier.padding(4.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Surface(
+            modifier = Modifier.size(32.dp),
+            color = Color.White,
+            shape = RoundedCornerShape(8.dp),
+            elevation = 4.dp
+        ) {
+            LottieAnimation(composition, lottieAnimatable.progress)
+        }
+    }
+}
+
+@Composable
+fun Stars(enabled: State<Boolean>, content: @Composable () -> Unit) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.stars))
+    val lottieAnimatable = rememberLottieAnimatable()
+
+    LaunchedEffect(enabled.value) {
+        lottieAnimatable.animate(
+            composition = composition,
+            speed = if (enabled.value) .8f else -8f,
+            iterations = if (!enabled.value) 0 else LottieConstants.IterateForever
+        )
+    }
+
+    Box(contentAlignment = Alignment.Center) {
+        Box(modifier = Modifier.padding(4.dp)) {
+            content()
+        }
+        LottieAnimation(
+            modifier = Modifier.matchParentSize(),
+            composition = composition,
+            progress = lottieAnimatable.progress
+        )
+    }
 }
